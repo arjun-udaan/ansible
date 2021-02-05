7c2c97deb61ec105842dc41369ee950687be1fd1
arjun-udaan <64679340+arjun-udaan@users.noreply.github.com>
2021-02-05 15:50:09 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-wget -O - https://raw.githubusercontent.com/arjun-udaan/ansible/main/ansible.sh | sudo bash -s  Blr-Harcx-D1-131
+echo H@rlurCX@43 | sudo -S wget -O - https://raw.githubusercontent.com/arjun-udaan/ansible/main/ansible.sh | sudo bash -s  Blr-Harcx-D1-137
